ae7825f4376462766305e32b0ef81a144446e3cb
Robert G <bluewhobert@mailbox.org>
2019-02-27 22:34:17 +0100

    extend existing unit tests

--- tests/erika_image_render_unittest.py
+++ tests/erika_image_render_unittest.py
@@ -10,24 +10,41 @@ from tests.erika_mock_unittest import assert_print_output
 # noinspection SpellCheckingInspection
 class RendererTest(unittest.TestCase):
 
-    def helper_test_ErikaImageRenderingStrategy(self, strategy):
+    def helper_test_ErikaImageRenderingStrategy_square(self, strategy):
         """test helper to verify rendering with the given strategy works"""
         with ErikaMock(6, 6) as my_erika:
             renderer = ErikaImageRenderer(my_erika, strategy)
             renderer.render_ascii_art_file('tests/test_resources/test_ascii_art_small.txt')
             assert_print_output(self, my_erika, ["abcdef", "ghijkl", "mnopqr", "stuvwx", "yzäöüß", "!?#'\"/"])
 
+    def helper_test_ErikaImageRenderingStrategy_high(self, strategy):
+        """test helper to verify rendering with the given strategy works"""
+        with ErikaMock(3, 12) as my_erika:
+            renderer = ErikaImageRenderer(my_erika, strategy)
+            renderer.render_ascii_art_file('tests/test_resources/test_ascii_art_small_high.txt')
+            assert_print_output(self, my_erika,
+                                ["abc", "def", "ghi", "jkl", "mno", "pqr", "stu", "vwx", "yzä", "öüß", "!?#", "'\"/"])
+
+    def helper_test_ErikaImageRenderingStrategy_wide(self, strategy):
+        """test helper to verify rendering with the given strategy works"""
+        with ErikaMock(9, 4) as my_erika:
+            renderer = ErikaImageRenderer(my_erika, strategy)
+            renderer.render_ascii_art_file('tests/test_resources/test_ascii_art_small_wide.txt')
+            assert_print_output(self, my_erika, ["abcdefghi", "jklmnopqr", "stuvwxyzä", "öüß!?#'\"/"])
+
     def testLineByLineErikaImageRenderingStrategy(self):
         """simple test that printing line by line works"""
         strategy = LineByLineErikaImageRenderingStrategy()
-        self.helper_test_ErikaImageRenderingStrategy(strategy)
-
+        self.helper_test_ErikaImageRenderingStrategy_square(strategy)
+        self.helper_test_ErikaImageRenderingStrategy_high(strategy)
+        self.helper_test_ErikaImageRenderingStrategy_wide(strategy)
 
     def testInterlacedErikaImageRenderingStrategy(self):
         """simple test that printing odd-then-even lines works"""
         strategy = InterlacedErikaImageRenderingStrategy()
-        self.helper_test_ErikaImageRenderingStrategy(strategy)
-
+        self.helper_test_ErikaImageRenderingStrategy_square(strategy)
+        self.helper_test_ErikaImageRenderingStrategy_high(strategy)
+        self.helper_test_ErikaImageRenderingStrategy_wide(strategy)
 
 
 def main():
