73a737ae214feec52bc068487d88aad79d2ac194
Robert G <bluewhobert@mailbox.org>
2019-02-27 20:32:22 +0100

    refactor rendering unit test; add InterlacedErikaImageRenderingStrategy

--- erika/erika_image_renderer.py
+++ erika/erika_image_renderer.py
@@ -32,3 +32,36 @@ class LineByLineErikaImageRenderingStrategy(ErikaImageRenderingStrategy):
                 line_without_newline = line.replace('\n', "")
                 self.output_device.print_ascii(line_without_newline)
                 self.output_device.crlf()
+
+
+class InterlacedErikaImageRenderingStrategy(ErikaImageRenderingStrategy):
+
+    def __init__(self):
+        ErikaImageRenderingStrategy.__init__(self)
+
+    def render_ascii_art_file(self, file_path):
+        with open(file_path, "r") as open_file:
+            lines = open_file.readlines()
+            line_count = len(lines)
+            moved = 0
+            for even in range(0, line_count, 2):
+                line_without_newline = lines[even].replace('\n', "")
+                self.output_device.print_ascii(line_without_newline)
+                self.output_device.crlf()
+                self.output_device.crlf()
+                moved += 2
+
+            # reset cursor to start of line
+            self.output_device.crlf()
+
+            # do not compensate the line that this adds - the extra line will position the cursor right where we want it
+            # self.output_device.move_up()
+
+            for lines_to_move_up in range(0, moved):
+                self.output_device.move_up()
+
+            for odd in range(1, line_count, 2):
+                line_without_newline = lines[odd].replace('\n', "")
+                self.output_device.print_ascii(line_without_newline)
+                self.output_device.crlf()
+                self.output_device.crlf()
--- tests/erika_image_render_unittest.py
+++ tests/erika_image_render_unittest.py
@@ -1,3 +1,5 @@
+# coding=utf-8
+
 import unittest
 
 from erika.erika_image_renderer import *
@@ -5,14 +7,27 @@ from tests.erika_mock import *
 from tests.erika_mock_unittest import assert_print_output
 
 
+# noinspection SpellCheckingInspection
 class RendererTest(unittest.TestCase):
-    def testRenderLineByLineConnect(self):
-        """simple test that printing line by line works"""
+
+    def helper_test_ErikaImageRenderingStrategy(self, strategy):
+        """test helper to verify rendering with the given strategy works"""
         with ErikaMock(6, 6) as my_erika:
-            rendering_strat = LineByLineErikaImageRenderingStrategy()
-            renderer = ErikaImageRenderer(my_erika, rendering_strat)
+            renderer = ErikaImageRenderer(my_erika, strategy)
             renderer.render_ascii_art_file('tests/test_resources/test_ascii_art_small.txt')
             assert_print_output(self, my_erika, ["abcdef", "ghijkl", "mnopqr", "stuvwx", "yzäöüß", "!?#'\"/"])
+
+    def testLineByLineErikaImageRenderingStrategy(self):
+        """simple test that printing line by line works"""
+        strategy = LineByLineErikaImageRenderingStrategy()
+        self.helper_test_ErikaImageRenderingStrategy(strategy)
+
+
+    def testInterlacedErikaImageRenderingStrategy(self):
+        """simple test that printing odd-then-even lines works"""
+        strategy = InterlacedErikaImageRenderingStrategy()
+        self.helper_test_ErikaImageRenderingStrategy(strategy)
+
 
 
 def main():
