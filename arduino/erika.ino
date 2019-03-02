95496006e7ae7a638084ec5f1502854190b85484
sirexeclp <sirexeclp@gmail.com>
2019-03-02 17:27:43 +0100

    use external interrupt to trigger on rts

--- arduino/erika.ino
+++ arduino/erika.ino
@@ -4,43 +4,45 @@
 
 #define PC_BAUD 9600
 
-#define RTS_PIN 3
+#define RTS_PIN 3 // must be interupt able
 #define ERIKA_RX 10
 #define ERIKA_TX 11
 #define ERIKA_BAUD 1200
 
 SoftwareSerial erika(ERIKA_RX, ERIKA_TX); // RX, TX
-bool wait = false;
+volatile bool wait = false;
 
 void setup()
 {
 	pinMode(RTS_PIN, INPUT);
 	Serial.begin(PC_BAUD);
 	erika.begin(ERIKA_BAUD);
+
+	//pullup needed ? 
+	//pinMode(RTS_PIN, INPUT_PULLUP);
+	attachInterrupt(digitalPinToInterrupt(RTS_PIN), onRTS, FALLING);
+}
+
+void onRTS()
+{
+	wait = false;
 }
 
 void loop()
 { // run over and over
-	int rtsState = digitalRead(RTS_PIN);
-
 	//continue on falling edge
-	if (rtsState == LOW && wait == false)
+	if (wait == false)
 	{
 		//data from pc in serial buffer ?
 		if (Serial.available())
 		{
+			//wait until next falling edge
+			wait = true;
 			//convert to raw (ddr-ascii)
 			int result = ascii2ddr[Serial.read()];
 			erika.write(result);
 			//Serial.println(result,HEX);
-			//wait until rts went high
-			wait = true;
 		}
-	}
-	else if (val == HIGH)
-	{
-		//rts was high, which means erika is ready for the next byte, when rts goes low again
-		wait = false;
 	}
 	//erika has data to send
 	if (erika.available())
