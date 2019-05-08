5ddb8781b83fa1947585889785edef689c30d50f
Robert G <bluewhobert@mailbox.org>
2019-05-08 23:11:46 +0200

    simple example: python builtin Thread class

--- example_code/threadstryout.py
+++ example_code/threadstryout.py
@@ -1,31 +1,49 @@
 from queue import Empty
 from queue import Full
 from queue import Queue
+from threading import Thread
 
 # all timeouts in seconds
 q = Queue(maxsize=10)
-q.put("item0", block=True, timeout=0.1)
-q.put("item1", block=True, timeout=0.1)
-q.put("item2")
-q.put("item3")
-q.put("item4")
-q.put("item5")
-q.put("item6")
-q.put("item7")
-q.put("item8")
-q.put("item9")
-try:
-    q.put("itemX", block=True, timeout=0.1)
-except Full as exception:
-    print("Exception ignored: Queue full")
+
+
+class ProducerThread(Thread):
+
+    def run(self):
+        for i in range(0, 100):
+            try:
+                q.put("item{}".format(i), block=True, timeout=0.1)
+                print(q.qsize())
+                print(q.queue)
+            except Full as exception:
+                print("Exception ignored: Queue full")
+
+
+class ConsumerThread(Thread):
+    def run(self):
+        for i in range(0, 100):
+            try:
+                q.get(block=True, timeout=0.1)
+                print(q.qsize())
+                print(q.queue)
+            except Empty as exception:
+                print("Exception ignored: Queue empty")
+
+
 print(q.qsize())
 print(q.queue)
 
-for i in range(0, 10):
-    q.get()
-try:
-    q.get(block=True, timeout=0.1)
-except Empty as exception:
-    print("Exception ignored: Queue empty")
+consumer_thread = ConsumerThread()
+producer_thread = ProducerThread()
+
+producer_thread.start()
+consumer_thread.start()
+
+print(q.qsize())
+print(q.queue)
+
+producer_thread.join()
+consumer_thread.join()
+
 print(q.qsize())
 print(q.queue)
