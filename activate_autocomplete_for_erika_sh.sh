5e63dbe6539b8d2dd47a8ebae9f28274b31bb1d8
Robert G <bluewhobert@mailbox.org>
2019-05-19 16:14:16 +0200

    CLI: add script to activate argument autocomplete

---
+++ activate_autocomplete_for_erika_sh.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+
+# ALTERNATIVE: activate tab completion for one script only (I have not tried this out successfully)
+#eval "$(register-python-argcomplete erika_sh)"
+
+echo "[ERIKA 3004] Activating script tab completion"
+echo "[ERIKA 3004] (see https://argcomplete.readthedocs.io/en/latest/#global-completion )"
+
+argcomplete_support_installed=$(type -P activate-global-python-argcomplete)
+if [[ "$?" -ne 0 ]]; then
+    echo "[ERIKA 3004] Unexpected error (type -P)"
+    exit 1
+fi
+if [[ -z "${argcomplete_support_installed}" ]]; then
+    echo "[ERIKA 3004] FAILED. Install python requirements: pip3 install -r requirements.txt"
+    exit 1
+fi
+
+activate-global-python-argcomplete 1>/dev/null 2>/dev/null
+if [[ "$?" -ne 0 ]]; then
+
+    echo "[ERIKA 3004] Direct installation FAILED."
+    echo "[ERIKA 3004] Instead, will generate a script instead..."
+    echo "[ERIKA 3004] activate-global-python-argcomplete --dest=- > /tmp/python-argcomplete.sh"
+    activate-global-python-argcomplete --dest=- > /tmp/python-argcomplete.sh
+    if [[ "$?" -ne 0 ]]; then
+        echo "[ERIKA 3004] Script generation FAILED as well. Exiting..."
+        exit 1
+    fi
+    echo "[ERIKA 3004] DONE"
+    echo "[ERIKA 3004] ...and copy it to the target directory then."
+    echo "[ERIKA 3004] THIS WILL REQUIRE SUDO ACCESS:"
+    echo "[ERIKA 3004] sudo cp /tmp/python-argcomplete.sh /etc/bash_completion.d"
+    sudo cp /tmp/python-argcomplete.sh /etc/bash_completion.d
+    if [[ "$?" -ne 0 ]]; then
+        echo "[ERIKA 3004] Copying the script FAILED. Exiting..."
+        exit 1
+    fi
+fi
+
+echo "[ERIKA 3004] tab completion successfully activated."
+
