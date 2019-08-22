a37cc956568d213058b685de0d178117bbb3dc46
Matthias Jacob <dev@matthiasjacob.net>
2019-08-22 16:40:20 +0200

    rewrite sanitization

--- erika/tcp_server.py
+++ erika/tcp_server.py
@@ -7,15 +7,7 @@ TCP_IP = '127.0.0.1'
 TCP_PORT = 2227
 BUFFER_SIZE = 1024  # Normally 1024, but we want fast response
 
-
-def sanitize_tweet(tweet_as_string):
-    allowed_characters = string.digits + string.ascii_letters + "@.,;:# ()_/?!\"§+%&=*-'äöüÄÖÜßéè°|$£µ^²³"
-    sanitized_tweet = tweet_as_string
-    sanitized_tweet = ''.join(c for c in sanitized_tweet if c in allowed_characters)
-    sanitized_tweet = sanitized_tweet.replace('@', "(at)")
-
-    return sanitized_tweet
-
+ALLOWED_CHARACTERS = string.digits + string.ascii_letters + "@.,;:# ()_/?!\"§+%&=*-'äöüÄÖÜßéè°|$£µ^²³"
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
@@ -26,6 +18,15 @@ while True:
     print('Connection address:', addr)
     data = conn.recv(BUFFER_SIZE)
     print("received data:", data)
-    print("sanitized data:", sanitize_tweet(data))
-    conn.send(data)  # echo
-    conn.close()
+
+    try:
+        sanitized_tweet = data.decode('utf-8')
+        sanitized_tweet = ''.join(c for c in sanitized_tweet if c in ALLOWED_CHARACTERS)
+        sanitized_tweet = sanitized_tweet.replace('@', "(at)")
+
+        print("sanitized data:", sanitized_tweet)
+        conn.send(data)  # echo
+    except UnicodeDecodeError as e:
+        conn.send("Invalid bytes. You must send UTF-8.\n".encode("utf-8"))
+    finally:
+        conn.close()
