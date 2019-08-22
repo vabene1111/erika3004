dddc5732bcf8ed2088ea4fcaa2df388277805461
Matthias Jacob <dev@matthiasjacob.net>
2019-08-22 14:07:47 +0200

    change port to 2227 (=CCCP phoneword)

--- erika/tcp_server.py
+++ erika/tcp_server.py
@@ -4,7 +4,7 @@ import socket
 
 
 TCP_IP = '127.0.0.1'
-TCP_PORT = 5005
+TCP_PORT = 2227
 BUFFER_SIZE = 20  # Normally 1024, but we want fast response
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
