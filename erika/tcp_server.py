02ed6470ccce34e5808ac96cdc8b8d809e660f5e
Matthias Jacob <dev@matthiasjacob.net>
2019-08-22 14:02:30 +0200

    tcp server accepts multiple connections

--- erika/tcp_server.py
+++ erika/tcp_server.py
@@ -11,12 +11,13 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
 s.listen(1)
 
-conn, addr = s.accept()
-print('Connection address:', addr)
-while 1:
-    data = conn.recv(BUFFER_SIZE)
-    if not data: break
-    print("received data:", data)
-    conn.send(data)  # echo
-conn.close()
+while True:
+    conn, addr = s.accept()
+    print('Connection address:', addr)
+    while True:
+        data = conn.recv(BUFFER_SIZE)
+        if not data: break
+        print("received data:", data)
+        conn.send(data)  # echo
+    conn.close()
 
