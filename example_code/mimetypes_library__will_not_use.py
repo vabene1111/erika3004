1a2a96b505e66ace0bc2bd8d9ed957f63406a3e6
Robert G <bluewhobert@mailbox.org>
2019-10-16 08:10:22 +0200

    commit experimental code as "learning test"

---
+++ example_code/mimetypes_library__will_not_use.py
@@ -0,0 +1,33 @@
+import mimetypes
+
+# Run from repository's main directory!
+basePath = '../..F'
+
+print("text")
+
+file_path = basePath + 'tests/test_resources/test_ascii_art.txt'
+
+mime = mimetypes.guess_type(file_path)
+print(mime)
+print()
+
+print("BMP")
+file_path = basePath + 'tests/test_resources/test_image_color.bmp'
+mime = mimetypes.guess_type(file_path)
+print(mime)
+print()
+
+print("PNG")
+file_path = basePath + 'tests/test_resources/ubuntu-logo32.png'
+mime = mimetypes.guess_type(file_path)
+print(mime)
+print()
+
+#  FAIL
+print("PNG - changed file extension to trick the library")
+file_path = basePath + 'tests/test_resources/ubuntu-logo32.png.renamedwithextension.txt'
+mime = mimetypes.guess_type(file_path)
+print(mime)
+print()
+
+
