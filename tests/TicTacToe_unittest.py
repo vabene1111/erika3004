41c0acf3705a2038216d2475968549b646311fcf
Robert G <bluewhobert@mailbox.org>
2019-12-06 19:47:01 +0100

    TicTacToe: test checking of winning condition

--- tests/TicTacToe_unittest.py
+++ tests/TicTacToe_unittest.py
@@ -1,10 +1,98 @@
 import unittest
 
+from erika.TicTacToe import Players
 from erika.TicTacToe import TicTacToe
 from erika.erika_mock import CharacterBasedErikaMock
 from tests.erika_mock_unittest import assert_print_output
 
 
+# pytest-based tests must be functions on top level
+def test_check_winner_beginning(mocker):
+    with CharacterBasedErikaMock(11, 11, False, inside_unit_test=True) as my_erika:
+        game = TicTacToe(my_erika)
+        game._print_initial_field()
+        game._cursor_to_start_position()
+        mocker.patch.object(game, '_won')
+        mocker.patch.object(game, '_tie')
+
+        game._check_winner()
+
+        assert not game._tie.called
+        assert not game._won.called
+
+
+def test_check_winner_win_erika(mocker):
+    with CharacterBasedErikaMock(11, 11, False, inside_unit_test=True) as my_erika:
+        game = TicTacToe(my_erika)
+        game._print_initial_field()
+        game._cursor_to_start_position()
+        mocker.patch.object(game, '_won')
+        mocker.patch.object(game, '_tie')
+        game.board[0][1] = Players.Player1.value
+        game.board[1][2] = Players.Player1.value
+        game.board[2][0] = Players.Player1.value
+        game.board[0][0] = Players.Erika.value
+        game.board[1][1] = Players.Erika.value
+        game.board[2][2] = Players.Erika.value
+
+        game._check_winner()
+
+        assert not game._tie.called
+        assert game._won.called
+        game._won.assert_called_with(Players.Erika.value)
+
+
+def test_check_winner_win_player(mocker):
+    with CharacterBasedErikaMock(11, 11, False, inside_unit_test=True) as my_erika:
+        game = TicTacToe(my_erika)
+        game._print_initial_field()
+        game._cursor_to_start_position()
+        mocker.patch.object(game, '_won')
+        mocker.patch.object(game, '_tie')
+        game.board[0][2] = Players.Player1.value
+        game.board[1][2] = Players.Player1.value
+        game.board[2][2] = Players.Player1.value
+        game.board[2][0] = Players.Erika.value
+        game.board[0][0] = Players.Erika.value
+        game.board[1][1] = Players.Erika.value
+
+        game._check_winner()
+
+        assert not game._tie.called
+        assert game._won.called
+        game._won.assert_called_with(Players.Player1.value)
+
+
+def test_check_winner_game_tied(mocker):
+    with CharacterBasedErikaMock(11, 11, False, inside_unit_test=True) as my_erika:
+        game = TicTacToe(my_erika)
+        game._print_initial_field()
+        game._cursor_to_start_position()
+        mocker.patch.object(game, '_won')
+        mocker.patch.object(game, '_tie')
+
+        # o o x
+        # x x o
+        # o x x
+        game.board[0][0] = Players.Erika.value
+        game.board[0][1] = Players.Erika.value
+        game.board[0][2] = Players.Player1.value
+
+        game.board[1][0] = Players.Player1.value
+        game.board[1][1] = Players.Player1.value
+        game.board[1][2] = Players.Erika.value
+
+        game.board[2][0] = Players.Erika.value
+        game.board[2][1] = Players.Player1.value
+        game.board[2][2] = Players.Player1.value
+
+        game._check_winner()
+
+        assert game._tie.called
+        assert not game._won.called
+
+
+# unittest-based tests must reside class inheriting from unittest
 class TicTacToeUnitTest(unittest.TestCase):
     def test_initial_playing_field(self):
         """Simple test that the initial playing field looks like a tic tac toe grid"""
@@ -35,11 +123,3 @@ class TicTacToeUnitTest(unittest.TestCase):
             any_o_on_field = any(['o' in line for line in my_erika.canvas])
             self.assertFalse(any_x_on_field, "player should not have moved yet")
             self.assertTrue(any_o_on_field, "erika should have moved")
-
-
-def main():
-    unittest.main()
-
-
-if __name__ == '__main__':
-    main()
