2095115bc298a6f5c60c57253e060279795d186d
Robert G <bluewhobert@mailbox.org>
2020-01-16 21:18:51 +0100

    rework run_manual_tests.sh; add test case for missing file param error condition

--- run_manual_tests.sh
+++ run_manual_tests.sh
@@ -32,36 +32,52 @@ set -e
 # print evvery executed command
 set -x
 
+echo "
+
+### Test top-level help ####"
 ./erika.sh -h
 
+echo "
+
+### Test demo ####"
 ./erika.sh demo -h
 ./erika.sh demo -d
 
+echo "
+
+### Test input from stdin ####"
+cat tests/test_resources/test_ascii_art.txt | ./erika.sh render_ascii_art -d
+set +e
+echo "Next step: Expecting exception message, no new window popping up"
+read
+./erika.sh render_ascii_art -d
+set -e
+
+echo "
+
+### Test render_ascii_art ####"
 ./erika.sh render_ascii_art -h
 ./erika.sh render_ascii_art -d -f ./tests/test_resources/test_ascii_art.txt
-cat tests/test_resources/test_ascii_art.txt | ./erika.sh render_ascii_art -d
 ./erika.sh render_ascii_art -d -f ./tests/test_resources/test_ascii_art.txt -s LineByLine
 ./erika.sh render_ascii_art -d -f ./tests/test_resources/test_ascii_art.txt -s Interlaced
 ./erika.sh render_ascii_art -d -f ./tests/test_resources/test_ascii_art.txt -s RandomDotFill
 ./erika.sh render_ascii_art -d -f ./tests/test_resources/test_ascii_art.txt -s PerpendicularSpiralInward
 ./erika.sh render_ascii_art -d -f ./tests/test_resources/test_ascii_art.txt -s ArchimedeanSpiralOutward
 
+echo "
+
+### Test render_image ####"
 ./erika.sh render_image -h
 ./erika.sh render_image -d -f ./tests/test_resources/test_image_color.bmp
 ./erika.sh render_image -d -f ./tests/test_resources/test_image_grayscale_1.bmp -s LineByLine
-./erika.sh render_image -d -f ./tests/test_resources/test_image_color.bmp -s Interlaced
-./erika.sh render_image -d -f ./tests/test_resources/test_image_color.bmp -s RandomDotFill
-./erika.sh render_image -d -f ./tests/test_resources/test_image_color.bmp -s PerpendicularSpiralInward
-./erika.sh render_image -d -f ./tests/test_resources/test_image_color.bmp -s ArchimedeanSpiralOutward
+./erika.sh render_image -d -f ./tests/test_resources/test_image_grayscale_2.bmp -s Interlaced
+./erika.sh render_image -d -f ./tests/test_resources/test_image_monochrome_1.bmp -s RandomDotFill
+./erika.sh render_image -d -f ./tests/test_resources/test_image_monochrome_2.bmp -s PerpendicularSpiralInward
+./erika.sh render_image -d -f ./tests/test_resources/test_image_grayscale_2.bmp -s ArchimedeanSpiralOutward
 
-./erika.sh render_image -h
-./erika.sh render_image -d -f ./tests/test_resources/test_image_color.bmp
-./erika.sh render_image -d -f ./tests/test_resources/test_image_color.bmp -s LineByLine
-./erika.sh render_image -d -f ./tests/test_resources/test_image_color.bmp -s Interlaced
-./erika.sh render_image -d -f ./tests/test_resources/test_image_color.bmp -s RandomDotFill
-./erika.sh render_image -d -f ./tests/test_resources/test_image_color.bmp -s PerpendicularSpiralInward
-./erika.sh render_image -d -f ./tests/test_resources/test_image_color.bmp -s ArchimedeanSpiralOutward
+echo "
 
+### Test tictactoe ####"
 ./erika.sh tictactoe -d
 
 echo """
