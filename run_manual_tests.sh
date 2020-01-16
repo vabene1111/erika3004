6cbcbbabc385d8a58b5fb1ea72ef6b8955ab0cd8
Robert G <bluewhobert@mailbox.org>
2020-01-16 20:36:33 +0100

    add manual test for piped input

--- run_manual_tests.sh
+++ run_manual_tests.sh
@@ -39,8 +39,7 @@ set -x
 
 ./erika.sh render_ascii_art -h
 ./erika.sh render_ascii_art -d -f ./tests/test_resources/test_ascii_art.txt
-# FIXME
-#cat tests/test_resources/test_ascii_art.txt | ./erika.sh render_ascii_art -d
+cat tests/test_resources/test_ascii_art.txt | ./erika.sh render_ascii_art -d
 ./erika.sh render_ascii_art -d -f ./tests/test_resources/test_ascii_art.txt -s LineByLine
 ./erika.sh render_ascii_art -d -f ./tests/test_resources/test_ascii_art.txt -s Interlaced
 ./erika.sh render_ascii_art -d -f ./tests/test_resources/test_ascii_art.txt -s RandomDotFill
