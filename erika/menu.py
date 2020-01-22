0687ed8b3c346630768645599c08b784fe8af809
vabene1111 <vabene1234@googlemail.com>
2020-01-22 17:38:23 +0100

    test15

--- erika/menu.py
+++ erika/menu.py
@@ -2,6 +2,7 @@ class Menu:
 
     def __init__(self, erika):
         self.erika = erika
+        self.menu_running = True
 
     def __enter__(self):
         return self
@@ -15,11 +16,11 @@ class Menu:
         self.erika.print_ascii("Menu")
         self.erika.move_down()
 
-        while True:
+        while self.menu_running:
             inp = self.erika.read()
             programs = {
-                "1": "tttblaxy",
-                "2": "test",
+                "1": "tic_tac_toe",
+                2: "test",
             }
 
             if programs.get(inp, None):
@@ -27,3 +28,4 @@ class Menu:
 
     def run_program(self, program):
         self.erika.print_ascii("Selected program " + program)
+        self.menu_running = False
