201edbdc6503098b4cb5c25c0cd90f69014a63f1
vabene1111 <vabene1234@googlemail.com>
2020-01-22 16:19:34 +0100

    menu first test

---
+++ erika/menu.py
@@ -0,0 +1,23 @@
+class Menu:
+
+    def __init__(self, erika):
+        self.erika = erika
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args):
+        self.erika.__exit__()
+
+    def start_menu(self):
+        self.erika.set_keyboard_echo(False)
+        self.erika.move_right()
+        self.erika.print_ascii("Menu")
+        self.erika.move_down()
+
+        while True:
+            if inp := self.erika.read():
+                self.erika.print_ascii("Entry selected was " + inp)
+            break
+
+        self.erika.set_keyboard_echo(True)
